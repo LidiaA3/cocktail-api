700d965fbe2f937f10bd161c35bd454b48cf80ce
Lidia A <lidia.lasy@gmail.com>
2023-04-24 21:15:44 +0200

    Try repair the bug

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 
 export default function Home() {
 
-    const [List, setList] = useState([]);
+    const [list, setList] = useState([]);
 
     useEffect(() => {
         fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
@@ -13,11 +13,12 @@ export default function Home() {
     return <>
         <h1>This is the home with Api</h1>
 
-        {List.map(item => {
+        {list.map(item => {
             <div className="card">
-                {console.log(item.strDrink)}
-                {/* <img src={item.strDrinkThumb.rendered} alt={item.strDrinkThumb.rendered} /> */}
-                <h3>{item.strDrink}</h3>
+                {console.log(item)}
+                <h3>{ item.strDrink }</h3>
+                <img src={ item.strDrinkThumb } alt={ item.strDrink } />
+                {console.log('This is the cocktail ' + item.strDrink)}
             </div>
         })}
     </>
